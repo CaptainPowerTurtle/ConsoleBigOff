9c691cdaece586d86707047341822c2c23a7fb5d
Jacob Larsen <jacob.larsen6401@hotmail.dk>
2018-11-15 10:41:56 +0100

    Add project files.

---
+++ ConsoleBigOff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleBigOff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
